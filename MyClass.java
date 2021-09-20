286c5f69937f15b24fc7fce534382da37866cea6
ronikvats <90868758+ronikvats@users.noreply.github.com>
2021-09-21 00:24:09 +0530

    Java Assignment Day 2 Submitted

---
+++ MyClass.java
@@ -0,0 +1,18 @@
+//  DAY 2 ASSIGNMENT solution
+package vats;
+import java.util.Scanner;
+public class MyClass {
+    public static void main(String[] args) {
+        int x, y, sum;
+        Scanner myObj = new Scanner(System.in); // Create a Scanner object
+        System.out.println("Type a number:");
+        x = myObj.nextInt(); // Read user input
+
+        System.out.println("Type another number:");
+        y = myObj.nextInt(); // Read user input
+
+        sum = x + y;  // Calculate the sum of x + y
+        System.out.println("Sum is: " + sum); // Print the sum
+    }
+}
+
